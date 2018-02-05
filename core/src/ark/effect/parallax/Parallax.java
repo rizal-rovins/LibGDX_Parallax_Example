da11c0d6d6ac592f6bfbffa52f258e03b52153d7
Ajul Raj <ajulrajar@gmail.com>
2018-02-06 00:54:19 +0530

    somewhat working.

--- core/src/ark/effect/parallax/Parallax.java
+++ core/src/ark/effect/parallax/Parallax.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
 import com.badlogic.gdx.graphics.g3d.decals.Decal;
 import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
 import com.badlogic.gdx.graphics.g3d.decals.GroupStrategy;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 
 public class Parallax extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -19,6 +20,9 @@ public class Parallax extends ApplicationAdapter {
 	Decal decal;
 	DecalBatch decalBatch;
 	PerspectiveCamera camera;
+	CameraInputController cameraInputController;
+
+	float prev_y;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -28,6 +32,8 @@ public class Parallax extends ApplicationAdapter {
 		camera.near =0.1f;
 		camera.far = 200f;
 
+		cameraInputController=new CameraInputController(camera);
+		Gdx.input.setInputProcessor(cameraInputController);
 		img = new Texture("badlogic.jpg");
 		decal=Decal.newDecal(10,10,new TextureRegion(img));
 		decal.setPosition(0,0,0);
@@ -35,19 +41,27 @@ public class Parallax extends ApplicationAdapter {
 		CameraGroupStrategy cameraGroupStrategy = new CameraGroupStrategy(camera);;
 		decalBatch=new DecalBatch(cameraGroupStrategy);
 		decalBatch.add(decal);
+
+		prev_y=Gdx.input.getAccelerometerY();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0.5f, 0.4f, 1, 0);
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
 
 		batch.begin();
 		camera.update();
-		decal.rotateX(1);
+
+		Gdx.app.log("pitch",String.valueOf(Gdx.input.getPitch()));
+		Gdx.app.log("roll",String.valueOf(Gdx.input.getRoll()));
+
+		/*decal.rotateX(1);
 		decal.rotateY(1);
-		decal.rotateZ(1);
+		decal.rotateZ(1);*/
+		decal.rotateY((prev_y-Gdx.input.getAccelerometerY())*4f);
+		prev_y=Gdx.input.getAccelerometerY();
 
 		decalBatch.add(decal);
 		decalBatch.flush();
